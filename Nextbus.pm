b8698c6aaab93b4b315807b48bb49af170290468
Peter H. Li <phli@cpan.org>
2004-07-14 06:26:48 +0000

    Import of PHLI/WebService-Nextbus-0.11 from CPAN.
    gitpan-cpan-distribution: WebService-Nextbus
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         PHLI/WebService-Nextbus-0.11.tar.gz
    gitpan-cpan-author:       PHLI
    gitpan-cpan-maturity:     released

--- Nextbus.pm
+++ Nextbus.pm
@@ -1,28 +1,29 @@
 package WebService::Nextbus;
-@ISA = qw(LWP::UserAgent);
 use 5.006;
 use strict;
 use warnings;
+use integer;
 use WebService::Nextbus::Agency;
 use LWP::UserAgent;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub new {
 	my $proto = shift;
 	my $class = ref($proto) || $proto;
 
 	my $self = {
+		# URL-building internals (basically #DEFINEs)
 		_domain => 'www.nextbus.com',
 		_site => {
-			_base => 'wireless/',
-			_region => 'miniRegion.shtml',
-			_agency => 'miniAgency.shtml',
-			_route => 'miniRoute.shtml',
-			_dir => 'miniDirection.shtml',
-			_stop => 'miniStop.shtml',
-			_to => 'miniToStop.shtml',
-			_predict => 'miniPrediction.shtml',
+			base => 'wireless/',
+			region => 'miniRegion.shtml',
+			agency => 'miniAgency.shtml',
+			route => 'miniRoute.shtml',
+			dir => 'miniDirection.shtml',
+			stop => 'miniStop.shtml',
+			to => 'miniToStop.shtml',
+			predict => 'miniPrediction.shtml',
 		},
 		_keys => {
 			region => 're',
@@ -32,28 +33,15 @@ sub new {
 			stop => 's',
 			to => 'ts',
 		},
+
+		# The real workhorses
 		_ua => new LWP::UserAgent,
 		_agencies => {},
 	};
 	bless ($self, $class);
 }
 
-sub _parseLinks {
-	my ($content, $getKey) = @_;
-	my $linkRegExp1 = '(<a href=[^>]+>[^<]*<\/a>)';
-	my $linkRegExp2 = '^<a href=["\']([^"\']*)["\'][^>]*>([^<]*)<\/a>';
-	my $keyRegExp = "$getKey=([^&]*)";
-
-	my $links = undef;
-	my @rawLinks = ($content =~ /$linkRegExp1/gi);
-	foreach my $link (@rawLinks) {
-		my ($url, $name) = ($link =~ /$linkRegExp2/i);
-		next unless my ($code) = ($url =~/$keyRegExp/);
-		$links->{$name} = $code;
-	}
-	return $links;
-}
-
+# To propagate a new Agency with screen scraped data
 sub buildAgency {
 	my $self = shift;
 	my ($nameCode) = @_;
@@ -69,91 +57,94 @@ sub buildAgency {
 	}
 }
 
-sub getStops {
+# Private internal functions for getting and processing data from website
+my $_parseLinks = sub {
 	my $self = shift;
-	my ($agency, $route, $dir) = @_;
+	my ($content, $getKey) = @_;
 
-	my $getKeys = '?' . $self->keys->{agency} . '=' . $agency . 
-		'&' . $self->keys->{route} . '=' . $route .
-		'&' . $self->keys->{dir} . '=' . $dir;
-	my $url = $self->baseURL . $self->dir . $getKeys;
+	my $linkRegExp1 = '(<a href=[^>]+>.*?<\/a>)';
+	my $linkRegExp2 = '^<a href=["\']([^"\']*)["\'][^>]*>' .
+		'(<[^>]+>)*' . '([^<]*)' . '(<\/[^>]+>)*<\/a>$';
+	my $keyRegExp = "$getKey=([^&]*)";
+
+	my @rawLinks = ($content =~ /$linkRegExp1/gi);
+	my $links = undef;
+	foreach my $link (@rawLinks) {
+		my ($url, undef, $name) = ($link =~ /$linkRegExp2/i);
+		next unless my ($code) = ($url =~/$keyRegExp/);
+		$links->{$name} = $code;
+	}
+	return $links;
+};
+
+my $_getKeys = sub {
+	my $self = shift;
+	my ($url, $getKey) = @_;
 
 	my $response = $self->ua->get($url);
 	if ($response->is_success) {
-		my $content = $response->content;
-		my $links = _parseLinks($content, $self->keys->{stop});
+		my $links = $self->$_parseLinks($response->content, $getKey);
 	}
+};
+
+# Screen scraping functions for getting data from website
+sub getStops {
+	my $self = shift;
+	my ($agency, $route, $dir) = @_;
+	my $getKeys = '?' . $self->keys->{agency} . '=' . $agency . 
+		'&' . $self->keys->{route} . '=' . $route .
+		'&' . $self->keys->{dir} . '=' . $dir;
+	my $url = $self->baseURL . $self->site->{stop} . $getKeys;
+	$self->$_getKeys($url, $self->keys->{stop});
 }
 
 sub getDirs {
 	my $self = shift;
 	my ($agency, $route) = @_;
-
 	my $getKeys = '?' . $self->keys->{agency} . '=' . $agency . 
 		'&' . $self->keys->{route} . '=' . $route;
-	my $url = $self->baseURL . $self->dir . $getKeys;
-
-	my $response = $self->ua->get($url);
-	if ($response->is_success) {
-		my $content = $response->content;
-		my $links = _parseLinks($content, $self->keys->{dir});
-	}
+	my $url = $self->baseURL . $self->site->{dir} . $getKeys;
+	$self->$_getKeys($url, $self->keys->{dir});
 }
 
 sub getRoutes {
 	my $self = shift;
 	my ($agency) = @_;
-
 	my $getKeys = '?' . $self->keys->{agency} . '=' . $agency;
-	my $url = $self->baseURL . $self->route . $getKeys;
-
-	my $response = $self->ua->get($url);
-	if ($response->is_success) {
-		my $content = $response->content;
-		my $links = _parseLinks($content, $self->keys->{route});
-	}
+	my $url = $self->baseURL . $self->site->{route} . $getKeys;
+	$self->$_getKeys($url, $self->keys->{route});
 }
 
 sub getAgencies {
 	my $self = shift;
 	my ($region) = @_;
-
 	my $getKeys = '?' . $self->keys->{region} . '=' . $region;
-	my $url = $self->baseURL . $self->agency . $getKeys;
-
-	my $response = $self->ua->get($url);
-	if ($response->is_success) {
-		my $content = $response->content;
-		my $links = _parseLinks($content, $self->keys->{agency});
-	}
+	my $url = $self->baseURL . $self->site->{agency} . $getKeys;
+	$self->$_getKeys($url, $self->keys->{agency});
 }
 
 sub getRegions {
 	my $self = shift;
-	my $url = $self->baseURL . $self->region;
-
-	my $response = $self->ua->get($url);
-	if ($response->is_success) {
-		my $content = $response->content;
-		my $links = _parseLinks($content, $self->keys->{region});
-	}
+	my $url = $self->baseURL . $self->site->{region};
+	$self->$_getKeys($url, $self->keys->{region});
 }
 
+# Initialize an Agency
 sub setupAgency {
 	my $self = shift;
 	my ($nameCode) = @_;
-
 	my $a = new WebService::Nextbus::Agency;
 	$a->nameCode($nameCode);
-
 	$self->agencies->{$nameCode} = $a;
 }
 
+# Doesn't really do much, but if website changes, will help us to adapt
 sub baseURL {
 	my $self = shift;
-	return 'http://' . $self->domain . '/' . $self->base;
+	return 'http://' . $self->domain . '/' . $self->site->{base};
 }
 
+# Standard methods for checking and resetting internals
 sub agencies {
 	my $self = shift;
 	if (@_) { %{$self->{_agencies}} = %{$_[0]} }
@@ -164,12 +155,6 @@ sub ua {
 	my $self = shift;
 	if (@_) { $self->{_ua} = shift }
 	return $self->{_ua};
-}
-
-sub tree {
-      my $self = shift;
-      if (@_) { %{$self->{_tree}} = %{$_[0]} }
-      return \%{$self->{_tree}};
 }
 
 sub domain {
@@ -190,86 +175,90 @@ sub keys {
       return \%{$self->{_keys}};
 }
 
-sub base {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_base}} = shift }
-	return $self->{_site}->{_base};
-}
-
-sub region {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_region}} = shift }
-	return $self->{_site}->{_region};
-}
-
-sub agency {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_agency}} = shift }
-	return $self->{_site}->{_agency};
-}
-
-sub route {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_route}} = shift }
-	return $self->{_site}->{_route};
-}
-
-sub dir {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_dir}} = shift }
-	return $self->{_site}->{_dir};
-}
-
-sub stop {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_stop}} = shift }
-	return $self->{_site}->{_stop};
-}
-
-sub to {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_to}} = shift }
-	return $self->{_site}->{_to};
-}
-
-sub predict {
-	my $self = shift;
-	if (@_) { ${$self->{_site}->{_predict}} = shift }
-	return $self->{_site}->{_predict};
-}
-
 1
 __END__
-# Below is stub documentation for your module. You better edit it!
 
 =head1 NAME
-
-WebService::Nextbus - Perl extension for blah blah blah
-
+      
+WebService::Nextbus - A screen scraper useful for propagating the data structure
+of WebService::Nextbus::Agency.
+      
+            
 =head1 SYNOPSIS
-
+      
   use WebService::Nextbus;
-  blah blah blah
+  $nb = new WebService::Nextbus;
+  $nb->buildAgency('sf-muni'); # Scraping the webpages repeatedly can take time
+  @stops = $nb->agencies->{'sf-muni'}->str2stopCodes('N', 'judah', 'Chu Dub');
 
+C<@stops> can now be used as valid GET arguments on the nextbus webpage.
+      
+ 
 =head1 DESCRIPTION
 
-Stub documentation for WebService::Nextbus, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+WebService::Nextbus can determine the relevant GET arguments for queries to the 
+Nextbus website (www.nextbus.com) by screen scraping.  
+WebService::Nextbus::Agency implements a basic data structure for storing and
+retrieving the information gleaned by this screen scraping.
 
-Blah blah blah.
+Once the proper GET code has been retrieved, a web useragent can use the
+argument to build a URL for the desired information.  This useragent function
+will probably eventually be incorporated into WebService::Nextbus.
+
+The screen scraping is done without any additional required HTML parser module.
+I did this to improve interoperability, but the parsing is therefore
+necessarily crude and perhaps not as fast as it could be (it uses RegExps 
+rather than a state machine).  This shouldn't be a major issue, however; 
+although running the initial screen scraping, with buildAgency for example, can
+be slow, you should be able to store the results (using Storable for example)
+and then retrieve them quickly.  This should work well since the data don't
+change all that frequently.
+
+For example:
+
+  # As above (use emery agency for example because it's smaller, faster)
+  use WebService::Nextbus;
+  $nb = new WebService::Nextbus;
+  $nb->buildAgency('emery'); # Scraping the webpages repeatedly can take time
+
+  # Now store the resulting agency, retrieve it, and dump its contents
+  use Storable;
+  store($nb->agencies->{'emery'}, 'emery.store');
+  $agency = retrieve('emery.store');
+  print $agency->routesAsString;
+
+  # Or store just the routes tree, retrieve it, and dump its contents
+  store($nb->agencies->{'emery'}->routes, 'emery_routes.store');
+  $agency = new WebService::Nextbus::Agency;
+  $agency->routes(retrieve('emery_routes.store'));
+  print $agency->routesAsString;
+
 
 =head2 EXPORT
 
-None by default.
+None by default; OO interface.
+
+
+=head1 REQUIRES
+
+Requires the L<LWP::UserAgent> module and the L<WebService::Nextbus::Agency> 
+package.
+Tests require the Test::More module.
 
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>a.u.thor@a.galaxy.far.far.awayE<gt>
+Peter H. Li<lt>phli@cpan.org<gt>
+
+
+=head1 COPYRIGHT
+
+Licensed by Creative Commons
+http://creativecommons.org/licenses/by-nc-sa/2.0/
+
 
 =head1 SEE ALSO
 
-L<perl>.
+L<WebService::Nextbus::Agency>, L<LWP::UserAgent>, L<perl>.
 
 =cut
--- test.pl
+++ test.pl
@@ -2,10 +2,13 @@ use Test::More;
 use strict;
 use warnings;
 
-BEGIN { plan tests => 3 };
+BEGIN { plan tests => 5 };
 BEGIN { use_ok('WebService::Nextbus') };
 
 my $nb = new WebService::Nextbus;
-isa_ok($nb, 'WebService::Nextbus', 'new agency');
+isa_ok($nb, 'WebService::Nextbus', 'new nextbus');
 
-can_ok($nb, qw(site));
+can_ok($nb, qw(buildAgency getStops getDirs getRoutes getAgencies getRegions setupAgency baseURL agencies ua domain site keys));
+
+isa_ok($nb->ua, 'LWP::UserAgent', 'new lwpUserAgent');
+isa_ok($nb->setupAgency(''), 'WebService::Nextbus::Agency', 'new agency');
